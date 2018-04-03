3ad1034aa3f52bfd7aa841f4815b017d66805a3c
Tomas Voslar <tomas.voslar@iflix.com>
2018-04-03 14:56:55 +0200

    Fix.

--- src/components/sports/sportForm.component.js
+++ src/components/sports/sportForm.component.js
@@ -1,75 +1,74 @@
-import {React, Component} from 'react'
+import React, { Component } from 'react'
 import SportService from '../../services/sport.service'
 import { Card, Header, Form, Checkbox, Input, Button } from 'semantic-ui-react'
 import { Link, Redirect } from 'react-router-dom'
 
 export default class SportFormComponent extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            sport: {},
-            redirect: undefined,
-          }
+    this.state = {
+      sport: {},
+      redirect: undefined,
+    }
+  }
+
+  async componentDidMount() {
+    const sportId = this.props.match.params.sportId
+    let sport = {}
+    if (sportId !== 'new') {
+      sport = await SportService.getSportById(sportId)
     }
 
-    async componentDidMount() {
-        const sportId = this.props.sport.params.sportId
-        let sport = {}
-        if (sportId !== 'new') {
-            sport = await SportService.getSportById(sportId)
-        }
-    
-        this.setState({
-          sport,
-        })
+    this.setState({
+      sport,
+    })
+  }
+
+  async saveForm() {
+    if (this.state.sport.id) {
+      await SportService.update(this.state.sport, this.state.sport.id)
+    } else {
+      await SportService.create(this.state.sport)
     }
 
-    async saveForm() {
-        if (this.state.sport.id) {
-          await SportService.update(this.state.sport, this.state.sport.id)
-        } else {
-          await SportService.create(this.state.sport)
-        }
-    
-        this.setState({
-          redirect: `/sports/${this.state.sport.id}`,
-        })
+    this.setState({
+      redirect: '/',
+    })
+  }
+
+  render() {
+    const { redirect } = this.state
+
+    if (redirect) {
+      return <Redirect to={redirect} />;
     }
 
-    render() {
-        const { redirect } = this.state
-    
-        if (redirect) {
-          return <Redirect to={redirect} />;
-        }
-    
-        return (
-          <div>
-            <Header as="h1">Přidat/Upravit sport</Header>
-            <Form onSubmit={() => this.saveForm()}>
-              <Form.Field>
-                <label>Český název</label>
-                <Input
-                  required
-                  placeholder="Český název sportu"
-                  value={this.state.sport.czName}
-                  onChange={event => this.setState({ sport: { ...this.state.sport, name: event.target.value } })}
-                />
-              </Form.Field>
-              <Form.Field>
-                <label>Anglický název</label>
-                <Input
-                  required
-                  placeholder="Anglický název sportu"
-                  value={this.state.sport.enName}
-                  onChange={event => this.setState({ sport: { ...this.state.sport, name: event.target.value } })}
-                />
-              </Form.Field>
-              <Button type="submit">Potvrdit změny</Button>
-            </Form>
-          </div>
-        )
-      }
-
+    return (
+      <div>
+        <Header as="h1">Přidat/Upravit sport</Header>
+        <Form onSubmit={() => this.saveForm()}>
+          <Form.Field>
+            <label>Český název</label>
+            <Input
+              required
+              placeholder="Český název sportu"
+              value={this.state.sport.czName}
+              onChange={event => this.setState({ sport: { ...this.state.sport, czName: event.target.value } })}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Anglický název</label>
+            <Input
+              required
+              placeholder="Anglický název sportu"
+              value={this.state.sport.engName}
+              onChange={event => this.setState({ sport: { ...this.state.sport, engName: event.target.value } })}
+            />
+          </Form.Field>
+          <Button type="submit">Potvrdit změny</Button>
+        </Form>
+      </div>
+    )
+  }
 }
