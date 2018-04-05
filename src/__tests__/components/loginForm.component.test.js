6d154b1c20efaca562b9bc82bf95125bb6a4e523
Tomáš Voslař <t.voslar@gmail.com>
2018-04-05 23:38:16 +0200

    Added tests, travis integration. (#11)
    * Added tests, travis integration.
    
    * Added coveralls.
    
    * Updated readme file.
    
    * Updated test config.

---
+++ src/__tests__/components/loginForm.component.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import LoginFormComponent from '../../components/loginForm.component'
+import renderer from 'react-test-renderer';
+
+describe('Login form component', () => {
+  it('renders without crashing', () => {
+    const component = renderer.create(<LoginFormComponent />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
