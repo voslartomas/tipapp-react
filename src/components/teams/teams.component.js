211ff58ac26861db872b1e619a4915f0ec089f77
Martin Rosenberg <nitram.rosenberg@gmail.com>
2018-04-05 23:23:39 +0200

    CRUD teams

--- src/components/teams/teams.component.js
+++ src/components/teams/teams.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TeamService from '../../services/team.service'
-import { Button, Image, Icon, Header, Table, Label } from 'semantic-ui-react'
+import { Button, Image, Icon, Header, Table, Label, Modal } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 export default class TeamsComponent extends Component {
   constructor(props) {
@@ -12,15 +13,31 @@ export default class TeamsComponent extends Component {
   }
 
   async componentDidMount() {
+    this.loadTeams()
+  }
+
+  async loadTeams() {
     const teams = await TeamService.getTeams(this.props.match.params.leagueId)
 
     this.setState({ teams })
   }
 
+  show = () => this.setState({ open: true })
+  handleDeleteConfirm = async (teamId) => {
+    await TeamService.delete(teamId)
+    this.loadMatches()
+  }
+  handleDeleteCancel = () => this.setState({ open: false })
+
   render() {
     return (
       <div>
         <Header as="h1">Týmy</Header>
+        <Link to={`/leagues/${this.props.match.params.leagueId}/teams/form/new`}>
+          <Button primary>
+            Přidat tým
+          </Button>
+        </Link>
         <Table celled>
           <Table.Header>
             <Table.Row>
@@ -35,6 +52,22 @@ export default class TeamsComponent extends Component {
               <Table.Row>
                 <Table.Cell>
                   <Label ribbon>{team.czName} {team.shortcut}</Label>
+                  <Link to={`/leagues/${this.props.match.params.leagueId}/teams/form/${team.id}`} style={{marginRight: '5px'}}>Upravit</Link>
+                <a href="#" onClick={this.show}>Smazat</a>
+                <Modal size='small' open={this.state.open} onClose={this.handleDeleteCancel}>
+                  <Modal.Header>
+                    Smazat ?
+                  </Modal.Header>
+                  <Modal.Content>
+                    <p>Chcete opravdu smazat tento tým ?</p>
+                  </Modal.Content>
+                  <Modal.Actions>
+                    <Button negative onClick={this.handleDeleteCancel}>
+                      Ne
+                    </Button>
+                    <Button positive onClick={() => this.handleDeleteConfirm(team.id)} icon='checkmark' labelPosition='right' content='Ano'/>
+                  </Modal.Actions>
+                </Modal>
                 </Table.Cell>
                 <Table.Cell>{team.league.name}</Table.Cell>
                 <Table.Cell>{team.sport.czName}</Table.Cell>
