15dd61a890add17d1e4bcc3132a009f45b7d7210
Martin Rosenberg <nitram.rosenberg@gmail.com>
2018-05-01 17:21:18 +0200

    Revert "Revert "added""
    This reverts commit aecac34039c0a0d2ea460e8ac002de3ba341e688.

---
+++ src/components/registerForm.component.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
+import api from '../helpers/api'
+import RegisterService from '../services/register.service'
+import { Redirect } from 'react-router-dom'
+
+export default class RegisterFormComponent extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: {},
+      redirect: undefined
+    }
+  }
+
+  async register() {
+    RegisterService.register(this.state.user)
+
+    this.setState({
+      redirect: '/'
+    })
+  }
+
+  render() {
+    const { redirect } = this.state
+
+    if (redirect) {
+      return <Redirect to={redirect} />;
+    }
+
+    return (
+      <div className="login-form">
+        <style>{`
+          body > div,
+          body > div > div,
+          body > div > div > div.login-form {
+            height: 100%;
+          }
+        `}
+        </style>
+        <Grid
+          textAlign="center"
+          style={{ height: '100%' }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" color="teal" textAlign="center">
+              Registrace
+            </Header>
+            <Form size="large">
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Křestní jméno"
+                  value={this.state.user.firstName}
+                  onChange={event => this.setState({ user: {...this.state.user, firstName: event.target.value }})}
+                />
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Příjmení"
+                  value={this.state.user.lastName}
+                  onChange={event => this.setState({ user: {...this.state.user, lastName: event.target.value }})}
+                />
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Uživatelské jméno"
+                  value={this.state.user.username}
+                  onChange={event => this.setState({ user: {...this.state.user, username: event.target.value }})}
+                />
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="E-Mail"
+                  value={this.state.user.email}
+                  onChange={event => this.setState({ user: {...this.state.user, email: event.target.value }})}
+                />
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Telefonní číslo"
+                  value={this.state.user.mobileNumber}
+                  onChange={event => this.setState({ user: {...this.state.user, mobileNumber: event.target.value }})}
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Heslo"
+                  type="password"
+                  value={this.state.user.password}
+                  onChange={event => this.setState({ user: {...this.state.user, password: event.target.value }})}
+                />
+                <Button color="teal" fluid size="large" onClick={() => this.register()}>Registrovat se<i style={{ marginLeft: '5px' }} className="sign in icon" /></Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    )
+  }
+}
