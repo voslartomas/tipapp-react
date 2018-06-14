855e8b2411aa45178a99a6e7f52a1c4de5ce9225
rosenbergm <nitram.rosenberg@gmail.com>
2018-06-14 09:13:45 +0200

    added password error

--- src/components/password.component.js
+++ src/components/password.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Header, Form, Checkbox, Input, Button, Message, Popup } from 'semantic-ui-react'
+import { Card, Header, Form, Checkbox, Input, Button, Message, Popup, Image, Modal } from 'semantic-ui-react'
 import UserService from '../services/user.service';
 import { Link, Redirect } from 'react-router-dom'
 
@@ -11,19 +11,19 @@ export default class PasswordComponent extends Component {
       passwordNew: "",
       passwordNewAgain: "",
       redirect: undefined,
-      open: false
+      error: false
     }
   }
 
   async saveForm() {
     if (this.state.passwordNew == this.state.passwordNewAgain) {
       await UserService.changePassword({password: this.state.passwordNew});
+      this.setState({
+        redirect: '/profile/',
+      })
     } else {
-      
+      this.setState({error: true})
     }
-    this.setState({
-      redirect: '/profile/',
-    })
   }
 
   render() {
@@ -58,6 +58,10 @@ export default class PasswordComponent extends Component {
             />
           </Form.Field>
           <Button type="submit">Potvrdit změnu</Button>
+          {this.state.error && <Message negative>
+            <Message.Header>Zadaná hesla se neshodují!</Message.Header>
+            <p>Zadejte je znovu tak, aby se hesla v obou polích shodovala.</p>
+          </Message>}
         </Form>
       </div>
     );
