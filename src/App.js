d4f08bffbef681dd89516d6a8f6937f259421c4a
Roman Talas <roman.talas@livesport.eu>
2021-05-19 08:20:31 +0200

    login fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import LoginFormComponent from './components/loginForm.component'
 import SecuredComponent from './components/secured.component'
 import './App.css'
@@ -10,21 +10,25 @@ import { Route } from 'react-router-dom'
 moment.locale('cs')
 
 export default function App() {
+  const loggedIn = () => localStorage.getItem('token') && localStorage.getItem('token').length;
   const [action, setAction] = useState();
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(loggedIn);
+
 
   const login = () => {
+    console.log('loging in')
     setAction('login');
+    setIsLoggedIn(loggedIn())
   }
 
   const logout = () => {
     console.log('loging out')
     localStorage.setItem('token', '')
+    setIsLoggedIn(loggedIn())
     window.location.pathname = ''
     setAction('logout');
   }
-
-  const isLoggedIn = localStorage.getItem('token') && localStorage.getItem('token').length > 0
 
   return (
     <div className="main">
